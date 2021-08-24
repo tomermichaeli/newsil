4c6c228de9ad1332a8ac4daf870a96b0837bb69d
Dani Shifer <dani.shifer@gmail.com>
2021-08-24 16:00:24 +0300

    fix layouts

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,22 +4,21 @@ import * as headerStyles from "../styles/components/header.module.scss"
 
 export default function Header(props) {
   return (
-    <header
-      className={headerStyles.header}
-    // className={`${headerStyles.header} ${props.page === 'info' &&
-    //   headerStyles.info_page}`}
-    >
-      <nav
-        className={headerStyles.header__nav}
-        role="navigation"
-        aria-label="main navigation"
-      >
+    <header className={headerStyles.header}>
+      <nav className={headerStyles.header__nav} role="navigation" aria-label="main navigation">
         <Link to="/">
           <h1>{props.title}</h1>
         </Link>
-        <hr style={{ color: "#e87979" }} />
+
         <div className={headerStyles.tweets}>
-          <a class="twitter-timeline" data-lang="he" data-height="2000" href="https://twitter.com/thenewsil?ref_src=twsrc%5Etfw"></a>
+          <a
+            aria-hidden="true"
+            aria-label="twitter-timeline"
+            className="twitter-timeline"
+            data-lang="he"
+            data-height="100%"
+            href="https://twitter.com/thenewsil?ref_src=twsrc%5Etfw"
+          />
         </div>
         <div>
           {/* <h1>
