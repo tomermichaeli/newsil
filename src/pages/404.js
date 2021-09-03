3a6026dc6f9a5b2e39d94757c6a76d10f96fa520
Tomer Michaeli <michaelitomer@gmail.com>
2021-09-03 12:04:04 +0300

    auto redirect from 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,11 +4,14 @@ import Layout from "../components/Layout"
 import * as notFoundStyles from '../styles/pages/404.module.scss'
 
 export default function NotFound() {
+    setTimeout(function(){
+        window.location.href = '/';
+     }, 5000);
     return (
         <Layout page="404" bgColor="inherit">
             <div className={notFoundStyles.notFound__container}>
                 <Link to="/">
-                    <h1>Sorry, couldn't find that page.</h1>
+                    <h1>הדף לא נמצא. לחצו למעבר לעמוד הראשי</h1>
                 </Link>
             </div>
         </Layout>
