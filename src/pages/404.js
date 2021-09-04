802dceccceaf7d1d150f9f4f704601f69869972f
Dani Shifer <dani.shifer@gmail.com>
2021-09-04 12:48:14 +0300

    update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,19 +1,24 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React, { useEffect } from "react"
+import { Link, navigate } from "gatsby"
 import Layout from "../components/Layout"
-import * as notFoundStyles from '../styles/pages/404.module.scss'
+import * as notFoundStyles from "../styles/pages/404.module.scss"
 
 export default function NotFound() {
-    setTimeout(function(){
-        window.location.href = '/';
-     }, 5000);
-    return (
-        <Layout page="404" bgColor="inherit">
-            <div className={notFoundStyles.notFound__container}>
-                <Link to="/">
-                    <h1>הדף לא נמצא. לחצו למעבר לעמוד הראשי</h1>
-                </Link>
-            </div>
-        </Layout>
-    )
+  useEffect(() => {
+    const handle = setTimeout(() => {
+      navigate("/")
+    }, 5000)
+
+    return () => clearTimeout(handle)
+  }, [])
+
+  return (
+    <Layout page="404" bgColor="inherit">
+      <div className={notFoundStyles.notFound__container}>
+        <Link to="/">
+          <h1>הדף לא נמצא. לחצו למעבר לעמוד הראשי</h1>
+        </Link>
+      </div>
+    </Layout>
+  )
 }
